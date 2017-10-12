d0244eab3354748255832d550165512d0a3df265
Peter Thor <peter.thor@omnifinity.se>
2017-10-12 13:46:46 +0200

    - Minor type casting updates. - Now printing build info - Added parameters to limit debugging output (--printkeypress, --printcoordinates) - Added timer that prints alive state to the console

--- Binary/OmnideckEmulator.bat
+++ Binary/OmnideckEmulator.bat
@@ -1,3 +1,3 @@
 REM Omnideck Emulator init script
 
-".\OmnideckEmulator.exe" --usexbox360gamepad 0 --usevrpnoutput 1
+".\OmnideckEmulator.exe" --usexbox360gamepad 0 --usevrpnoutput 1 --printkeypress 0 --printcoordinates 0
