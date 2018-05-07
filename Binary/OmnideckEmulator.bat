5ef2ed2ac9cba8ae3f834e90f421995bfd52b0c7
peter.thor@omnifinity.se <peter.thor@omnifinity.se>
2018-05-07 22:10:15 +0200

    OmnideckEmulator executable for API v1.17.5.

--- Binary/OmnideckEmulator.bat
+++ Binary/OmnideckEmulator.bat
@@ -1,3 +1,7 @@
 REM Omnideck Emulator init script
 
-".\OmnideckEmulator.exe" --usexbox360gamepad 0 --usevrpnoutput 1 --printkeypress 0 --printcoordinates 0
+REM Example to show key presses etc
+REM ".\OmnideckEmulator.exe" --printkeypress 1 --printcoordinates 1
+
+REM Example that disables xbox 360 gamepad and does not print any keys/coordinates
+".\OmnideckEmulator.exe" --usexbox360gamepad 0 --printkeypress 0 --printcoordinates 0
