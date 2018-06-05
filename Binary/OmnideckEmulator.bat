147398c271d60533b6679621c5990f3b6928d251
peter.thor@omnifinity.se <peter.thor@omnifinity.se>
2018-06-05 10:25:28 +0200

    Added parameter "--movespeed" to allow user to change default max speed. Ensured that when user is pressing the left analog stick on the XBox 360 to its furthest location it will lead to a movement speed of 1.0 m/s by defalt. If user is pressing the "A"-button on the XBox 360 controller the speed will be multiplied by 2.0 (note that the API caps the max movement speed at around 3.0 m/s, thus the maximum input can be 1.5m/s).

--- Binary/OmnideckEmulator.bat
+++ Binary/OmnideckEmulator.bat
@@ -3,5 +3,6 @@ REM Omnideck Emulator init script
 REM Example to show key presses etc
 REM ".\OmnideckEmulator.exe" --printkeypress 1 --printcoordinates 1
 
-REM Example that disables xbox 360 gamepad and does not print any keys/coordinates
-".\OmnideckEmulator.exe" --usexbox360gamepad 0 --printkeypress 0 --printcoordinates 0
+REM Example that DISABLES the XBox 360 gamepad as input (it is enabled by default) and does not print any keys/coordinates
+REM also using a max movement speed of 1.0 m/s. If you press A on the XBox 360 controller the speed will double.
+".\OmnideckEmulator.exe" --usexbox360gamepad 0 --printkeypress 0 --printcoordinates 0 --movespeed 1.0
