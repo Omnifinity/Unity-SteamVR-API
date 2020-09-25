01e9a2d0bb3cc90e3d051440bf28dd059b04e87c
peter.thor@omnifinity.se <peter.thor@omnifinity.se>
2020-09-25 12:59:13 +0200

    Patch, added binary for updated Omnideck Emulator executable (was forgotten in previous commit).

--- Binary/OmnideckEmulator.bat
+++ Binary/OmnideckEmulator.bat
@@ -1,8 +1,5 @@
 REM Omnideck Emulator init script
 
-REM Example to show key presses etc
-REM ".\OmnideckEmulator.exe" --printkeypress 1 --printcoordinates 1
+REM ".\OmnideckEmulator.exe" --usexbox360gamepad 0 --usevrpnoutput 1 --printkeypress 0 --printcoordinates 0
 
-REM Example that DISABLES the XBox 360 gamepad as input (it is enabled by default) and does not print any keys/coordinates
-REM also using a max movement speed of 1.0 m/s. If you press A on the XBox 360 controller the speed will double.
-".\OmnideckEmulator.exe" --usexbox360gamepad 0 --printkeypress 0 --printcoordinates 0 --movespeed 1.0
+".\OmnideckEmulator.exe" --usexbox360gamepad 0 --usevrpnoutput 1 --usevrpninput 1 --printkeypress 0 --printcoordinates 0
